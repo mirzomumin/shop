d0517f9e75e6afdeee7bf75951bb2011470d8add
Mirzomumin <mirzomumin@list.ru>
2024-04-16 12:05:31 +0400

    Add extra test to cover product list with query param

--- tests/test_products/test_product_list.py
+++ tests/test_products/test_product_list.py
@@ -107,3 +107,53 @@ async def test_product_list_with_params_success(ac: AsyncClient):
     assert product_data["price"] == str(product.price)
     assert product_data["is_available"] == product.is_available
     assert product_data["category_id"] == product.category_id
+
+
+async def test_product_list_with_params_empty(ac: AsyncClient):
+    category_schema = CreateCategorySchema(name="Beverages")
+    category = await create_category(category=category_schema)
+    b_file = BytesIO(open("/home/mirzomumin/me.jpg", mode="rb").read())
+    image_file = UploadFile(
+        file=b_file, filename="image.jpg", headers={"content-type": "image/jpg"}
+    )
+    product_schema = CreateProductSchema(
+        name="Green Tea",
+        image=image_file,
+        description=(
+            "Lorem Ipsum is simply dummy text of "
+            "the printing and typesetting industry. Lorem Ipsum "
+            "has been the industry's standard dummy text ever since "
+            "the 1500s, when an unknown printer took a galley of type"
+            " and scrambled it to make a type specimen book."
+        ),
+        price=Decimal(109.99),
+        is_available=True,
+        category_id=category.id,
+    )
+    await create_product(product=product_schema)
+
+    category_schema = CreateCategorySchema(name="IT")
+    category_2 = await create_category(category=category_schema)
+    b_file = BytesIO(open("/home/mirzomumin/me.jpg", mode="rb").read())
+    image_file = UploadFile(
+        file=b_file, filename="image.jpg", headers={"content-type": "image/jpg"}
+    )
+    product_schema = CreateProductSchema(
+        name="CRM system",
+        image=image_file,
+        description=(
+            "CRM SYSTEM is simply dummy text of "
+            "the printing and typesetting industry. Lorem Ipsum "
+            "has been the industry's standard dummy text ever since "
+            "the 1500s, when an unknown printer took a galley of type"
+            " and scrambled it to make a type specimen book."
+        ),
+        price=Decimal(109.99),
+        is_available=True,
+        category_id=category_2.id,
+    )
+    await create_product(product=product_schema)
+    response = await ac.get("/products/", params={"category_id": 3000})
+
+    assert response.status_code == 200
+    assert len(response.json()) == 0
